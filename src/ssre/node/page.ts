8bece129d687a9b427653ea209e5c01d0d16c146
L2NE-dev <conway24b@gmail.com>
2026-09-07 16:01:05 +0700

    Last steps...

--- src/ssre/node/page.ts
+++ src/ssre/node/page.ts
@@ -55,7 +55,7 @@ export const renderPage = (options: PageOptions): string => {
 <html lang="${escapeHtml(options.lang ?? "en")}">
 <head>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, interactive-widget=overlays-content">
 <title>${title}</title>
 ${options.head ?? ""}
 ${styleTags(options.styles)}
